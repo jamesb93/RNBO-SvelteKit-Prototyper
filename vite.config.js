d585e53a1f5e4118f5f5b3a609f1bf55180b59cd
James Bradbury <github@jamesbradbury.net>
2023-03-27 19:51:44 +0800

    remove tree shaking

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		rollupOptions: {
+			treeshake: false
+		}
+	}
 };
 
 export default config;
